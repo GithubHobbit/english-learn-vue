1f2f2c60da39d4f48f40902cee431dfefde02ef5
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2022-04-14 21:07:14 +0500

    commit

--- src/main.js
+++ src/main.js
@@ -36,8 +36,6 @@ if (token) {
   }
 
   app.config.globalProperties.$http.defaults.headers = {
-    'Cache-Control': null,
-    'X-Requested-With': null,
     'Authorization': 'Bearer ' + token,
   };
 
