33b479fe138ecd12bdb13531407f41df290e150e
Alex <aaaa.aaaa.aaa.aaaa@mail.ru>
2022-06-17 01:14:44 +0500

    commit

--- src/services/generic.service.js
+++ src/services/generic.service.js
@@ -2,10 +2,6 @@ import axios from 'axios';
 
 const API_URL = process.env.VUE_APP_BACKEND_URL;
 export const request = async ({ url, method, data, headers }) => {
-  console.log('data');
-  console.log(data);
-  console.log('api');
-  console.log(API_URL);
   const response = await axios({
     url: `${API_URL}/${url}`,
     data,
@@ -13,11 +9,5 @@ export const request = async ({ url, method, data, headers }) => {
     headers,
   });
 
-  console.log(response);
-
   return response.data;
 };
-// const keys = Object.keys(err.response.data);
-// console.log(keys);
-// const values = Object.values(err.response.data);
-// console.log(values);
